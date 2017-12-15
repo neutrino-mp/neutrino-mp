3263232a4f87eb7e84cd6c54bfd4b38235610c4c
svenhoefer <svenhoefer@svenhoefer.com>
2017-12-15 23:20:55 +0100

    - settings_manager: remember last used directory
     Conflicts:
    	data/locale/deutsch.locale
    	data/locale/english.locale
    	src/gui/settings_manager.cpp
    
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2279,6 +2279,7 @@ const char * locale_real_names[] =
 	"servicemenu.scants",
 	"servicemenu.update",
 	"settings.backup",
+	"settings.backup_dir",
 	"settings.backup_failed",
 	"settings.help",
 	"settings.menu_hints",
